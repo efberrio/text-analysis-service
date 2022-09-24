8556404f34e63c0cbc2eef04340e8983caf18b71
Fabio Berrio <efberrio@gmail.com>
2022-09-23 22:28:08 -0500

    [#1]-Create initial project configuration

---
+++ src/main/java/com/appgate/textanalysis/TextAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.textanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/textanalysis/TextAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.textanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
